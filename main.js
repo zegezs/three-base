4f7c4c7f5c29bf53da296905673556d95580c8f8
lifuya <lifuya@imotorlinx.com>
2024-04-19 18:01:58 +0800

    feat(version): 代码提交

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import * as THREE from 'three';
 // import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 import crate from './crate.gif';
 import cat from './test.png'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0, 5, 10);
 camera.lookAt(scene.position);
 
@@ -41,7 +43,6 @@ document.body.appendChild( renderer.domElement );
 // 创建纹理
 const texture = new THREE.TextureLoader().load(cat);
 texture.colorSpace = THREE.SRGBColorSpace;
-console.log('textture', texture);
 // 创建平面
 // const plane = new THREE.PlaneGeometry( 5, 5);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -69,7 +70,9 @@ camera.position.z = 5;
 	false,            // aClockwise
 	0                 // aRotation
 );
-const points = curve.getPoints(500);
+
+const curvePath = new THREE.LineCurve3(new THREE.Vector3(0, 0, 0), new THREE.Vector3(5, 0, 0)) // 创建一个轨迹路线
+const points = curvePath.getPoints(5); //要将曲线分成的段数
 const geometryCurve = new THREE.BufferGeometry().setFromPoints(points);
 const materialCurve = new THREE.LineBasicMaterial({ color: 0xff0000 });
 
@@ -77,18 +80,47 @@ const ellipse = new THREE.Line(geometryCurve, materialCurve);
 scene.add(ellipse);
 scene.add(mesh);
 
+
+const loader = new GLTFLoader();
+const dracoLoader = new DRACOLoader(); //
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/'); 
+dracoLoader.setDecoderConfig({ type: 'js' }); //使用js方式解压
+dracoLoader.preload(); //初始化_initDecoder 解码器
+loader.load(
+	'https://shunos-iot.oss-cn-qingdao.aliyuncs.com/dm/-1/bcad19d9a07d483eb72d1b535065a415.gltf',
+	function (gltf) {
+		const model = gltf.scene;
+		scene.add(model);
+		camera.lookAt(gltf.scene.position);
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+		
+		// gltf.scene.position.set(0, 0, 0);
+	},
+	function (xhr) {
+		console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+	},
+	function (error) {
+		console.log('An error happened', error);
+	}
+)
+
+
 let time = 0;
 function animate() {
 	requestAnimationFrame( animate );
 	controls.update()
 	// cube.rotation.x += 0.01;
 	// cube.rotation.y += 0.01;
-  time += 0.01;
-            const t = (time % 1) / 1; // 正规化时间t到0到1之间
-            const point = curve.getPoint(t); // 根据时间t获取曲线上的位置
-            const tangent = curve.getTangent(t);
-            mesh.position.set(point.x, point.y, 0); // 更新立方体位置到曲线上
-            mesh.lookAt(point.add(tangent)); // 
+  time += 0.001;
+	const t = (time % 1) / 1; // 正规化时间t到0到1之间
+	const point = curvePath.getPoint(t); // 根据时间t获取曲线上的位置
+	const tangent = curve.getTangent(t);
+	mesh.position.set(point.x, point.y, 0); // 更新立方体位置到曲线上
+	// mesh.lookAt(point.add(tangent)); // 
 	renderer.render( scene, camera );
 }
 
